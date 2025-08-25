8ea9d53e576fdbcb60828af2c44faa8e7fef80e9
vaishnavi369codeit <156186064+vaishnavi24pixel@users.noreply.github.com>
2025-08-25 11:29:56 +0530

    Update Backend Test .js

--- Backend Test .js
+++ Backend Test .js
@@ -16,12 +16,7 @@ app.post("/:url/:validity/:shorturl",(req,res)=>{
         "expiry":"2025-01-01T00:3000Z"
     })
 });
+app.use("/",getlink);
+app.use("/",defaultgen);
 
 
-//"clientID": "322c87e0-8946-4544-9030-d050d0ce3cfd",
-   // "clientSecret": "NtzkhGsgaqrrQwCT"
-   /**{
-    "token_type": "Bearer",
-    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJ2YXNoMjJjc0BjbXJpdC5hYy5pbiIsImV4cCI6MTc1NjEwMDY0MCwiaWF0IjoxNzU2MDk5NzQwLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiOGE5NjVmNzgtMmJmYy00MDZmLWE0ZDYtZGRiYTc0YzViYWNmIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidmFpc2huYXZpIHNoYXJtYSIsInN1YiI6IjMyMmM4N2UwLTg5NDYtNDU0NC05MDMwLWQwNTBkMGNlM2NmZCJ9LCJlbWFpbCI6InZhc2gyMmNzQGNtcml0LmFjLmluIiwibmFtZSI6InZhaXNobmF2aSBzaGFybWEiLCJyb2xsTm8iOiIxY3IyMmNzMjA3IiwiYWNjZXNzQ29kZSI6InlVVlFYSyIsImNsaWVudElEIjoiMzIyYzg3ZTAtODk0Ni00NTQ0LTkwMzAtZDA1MGQwY2UzY2ZkIiwiY2xpZW50U2VjcmV0IjoiTnR6a2hHc2dhcXJyUXdDVCJ9.XejX2tYKXbza_zCDBIxKjaFpNYkpwc6dj_aCzBIkJE8",
-    "expires_in": 1756100640
-} */
